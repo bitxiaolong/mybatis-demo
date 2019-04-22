dd4afb680cda696e9b49dd5d3abf6ab7d519c12c
bitxiaolong <48685119+bitxiaolong@users.noreply.github.com>
2019-04-22 10:51:24 +0800

    Delete Text.java

--- spring-mybatis/src/main/java/Text.java
+++
@@ -1,9 +0,0 @@
-import com.dao.Appconfig;
-
-public class Text {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Appconfig.class);
-        WebsitesService service = annotationConfigApplicationContext.getBean(WebsitesService.class);
-        service.query();
-    }
-}
